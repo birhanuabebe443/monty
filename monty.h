6252f6a69f4a7442b16569000c9b831fe189c077
Abebe Birhanu <105628415+birhanuabebe443@users.noreply.github.com>
2022-10-10 01:03:46 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,11 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 /*--- Struct Definitions ---*/
 extern int push_arg;
